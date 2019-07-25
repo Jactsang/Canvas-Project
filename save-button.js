2dd1abbe8702f993e03e07ad44c0cb487b280c9e
Jactsang <jac.tsang.work@gmail.com>
2019-07-25 10:30:20 +0800

    fix download function

--- save-button.js
+++ save-button.js
@@ -1,12 +1,11 @@
 var pic=1;
 
-function download() {
-    pic++;
-    $('.buttons a').attr("download", `pic-${pic}.png`);
-    var download = document.getElementById("download");
-    var image = document.getElementById("canvas-real").toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
-    download.setAttribute("href", image);
-    //download.setAttribute("download","archive.png");
-    $('body').css("cursor","url('./Assets/brush-01.png'), auto");
-    }
+    function download() {
+        pic++;
+        var download = document.getElementById("download");
+        var image = document.getElementById("canvas-real").toDataURL("image/png")
+            .replace("image/png", "image/octet-stream");
+        download.setAttribute("download", `pic-${pic}.png`);
+        download.setAttribute("href", image);
+        //download.setAttribute("download","archive.png");
+        }
